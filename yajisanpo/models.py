f2cf21f9638ecea52417487053ccf958539a5a4c
morishun1112 <morishi11129@gmail.com>
2021-08-10 18:01:55 +0900

    models 1

--- yajisanpo/models.py
+++ yajisanpo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class StoreType(models.Model)
+    name = models.CharField(max_length=100,verbose_name="お店の種類")
+    def __str__(self):
+        return self.name
+
+class Log(models.Model)
+    name = models.CharField(max_length=100,verbose_name="履歴")
+    def __str__(self):
+        return self.name
+
+from django.contrib import admin
+from yajisanpo.models import StoreType
+from yajisanpo.models import Log
+
+admin.site.register(StoreType,Log)
