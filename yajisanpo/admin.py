1b45818596c045573224d7c3dcfa7a57b3db9781
morishun1112 <morishi11129@gmail.com>
2021-08-31 17:43:55 +0900

    admin 1

--- yajisanpo/admin.py
+++ yajisanpo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from yajisanpo.models import Director,Movie,log
 
-# Register your models here.
+admin.site.register(Director)
+admin.site.register(Movie)
+admin.site.register(Log)
