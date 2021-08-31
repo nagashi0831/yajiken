c0ff95af9ec2fae668791631ed9b4d2e4c5faca4
長島壮汰 <gmc_sou.custam@icloud.com>
2021-08-31 17:45:05 +0900

    URLをせっていしました

--- yajisanpo/urls.py
+++ yajisanpo/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'yajisanpo' #アプリ名
 urlpatterns = [
-    path('', views.TopView.as_view(), name='top'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('movie/<int:pk>/', views.MovieDetailViews.as_view(), name='movie_detail'),
+    path('register/director/', views.RegisterDirectorView.as_view(), name='registerdirector'),
+    path('register/movie/', views.RegisterMovieView.as_view(), name='registermovie'),
+    path('writing/log/', views.WritingLogView.as_view(), name='writinglog'),
 ]
